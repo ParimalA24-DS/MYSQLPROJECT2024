420f7ea22d518ad8300c913c699e00f649d3c024
Parimal_A Data_Analyst <parimalautade24@gmail.com>
2024-07-27 22:03:47 +0530

    Update 3.Walmart-General-QA.sql

--- 2.MySQL-Walmart-Sales-Analysis/3.Walmart-General-QA.sql
+++ 2.MySQL-Walmart-Sales-Analysis/3.Walmart-General-QA.sql
@@ -1,1 +1,17 @@
-
+SELECT * FROM walmart.sales;
+-- -- --------------------------------------------------------------------
+-- ---------------------------- General questions ------------------------------
+-- 1.How many unique cities does the data have?
+-- -----------------------------------------------------------------------
+SELECT distinct City FROM SALES;     # Yangon ,Mandalay ,Naypyitaw:3 cities 
+  
+-- 2. In which city is each branch?-- 
+SELECT 
+	DISTINCT city,
+    branch
+FROM sales;
+-- -------
+SELECT 
+	 city,
+    branch
+FROM sales;
